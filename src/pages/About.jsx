e9754032b7431ffc27b346ae6c368a9f5a43b10e
Hajarivelo1 <93727504+Hajarivelo1@users.noreply.github.com>
2023-03-21 18:00:30 +0300

    Premier Commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h2 className="section-title">about page</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id facilis
+        vitae reprehenderit veniam, esse nobis sapiente excepturi laboriosam
+        voluptatem unde nihil sunt numquam ducimus ea! Quam vitae quaerat sit
+        ex, voluptas harum eum. Itaque assumenda voluptatum fugiat iste at
+        illum?
+      </p>
+    </section>
+  );
+};
+
+export default About;
